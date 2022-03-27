6514eb4ce28bc00fcdd04f9dcfe58cbb15d02fb2
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2022-03-27 03:42:43 +0200

    Added Python call inside C# code...

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+public class Program
+{
+    public static void runImgPy()
+    {
+        Process process = new Process();
+        ProcessStartInfo processStartInfo = new ProcessStartInfo();
+        processStartInfo.FileName = @"C:\Python310\python.exe";
+        processStartInfo.Arguments = Environment.CurrentDirectory + @"\img.py";
+        processStartInfo.RedirectStandardOutput = true;
+        processStartInfo.UseShellExecute = false;
+        processStartInfo.CreateNoWindow = true;
+        process.StartInfo = processStartInfo;
+        process.Start();
+    }
+
+    public static void Main(string[] args) {
+        runImgPy();
+    }
+}

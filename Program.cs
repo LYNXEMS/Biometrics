c78e8d337e4e505accbe4c50a3a660f803b50228
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2022-03-27 17:24:56 +0200

    Rewritten pixel structure in C#, removed C implementation, etc. ( mainly in Program.cs).

--- Program.cs
+++ Program.cs
@@ -1,22 +1,35 @@
 ﻿using System;
 using System.Diagnostics;
-
-public class Program
+namespace Biometrics
 {
-    public static void runImgPy()
+    public class Program
     {
-        Process process = new Process();
-        ProcessStartInfo processStartInfo = new ProcessStartInfo();
-        processStartInfo.FileName = @"C:\Python310\python.exe";
-        processStartInfo.Arguments = Environment.CurrentDirectory + @"\img.py";
-        processStartInfo.RedirectStandardOutput = true;
-        processStartInfo.UseShellExecute = false;
-        processStartInfo.CreateNoWindow = true;
-        process.StartInfo = processStartInfo;
-        process.Start();
-    }
+        public static void runImgPy()
+        {
+            Process process = new Process();
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = @"C:\Python310\python.exe";
+            processStartInfo.Arguments = Environment.CurrentDirectory + @"\img.py";
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.CreateNoWindow = true;
+            process.StartInfo = processStartInfo;
+            process.Start();
+        }
 
-    public static void Main(string[] args) {
-        runImgPy();
+        public static void test()
+        {
+            pixel p;
+            p.x = 0;
+            p.y = 0;
+            p.value = (char)0;
+            Console.WriteLine(p.x + "\n" + p.y + "\n" + Convert.ToInt16(p.value));
+        }
+
+        public static void Main(string[] args)
+        {
+            //test();
+            //runImgPy();
+        }
     }
 }
---
+++ pixel.cs
@@ -0,0 +1,11 @@
+﻿namespace Biometrics
+{
+    //to do
+    //lookup table //LUT
+    public struct pixel
+    {
+        public uint x;
+        public uint y;
+        public char value;
+    }
+}

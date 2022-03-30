66ea4f804468ee6ea18f6c91da382af79311bff3
Mateusz Pietrzak <mateuszpietrzakpl@gmail.com>
2022-03-30 13:12:37 +0200

    Convert and bases

---
+++ Helpers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biometrics
+{
+    internal class Helpers
+    {
+		static Color White = Color.FromArgb(255, 255, 255);
+		static Color Black = Color.FromArgb(0, 0, 0);
+
+		public static void Binarize(Bitmap tImage)
+		{
+			{
+				for (int x = 0; x < tImage.Width; x++)
+				{
+					for (int y = 0; y < tImage.Height; y++)
+					{
+						Color tCol = tImage.GetPixel(x, y);
+						if (tCol.R > 127) tImage.SetPixel(x, y, White);
+						else tImage.SetPixel(x, y, Black);
+					}
+				}
+			}
+		}
+
+		public static void Greyscale(Bitmap tImage)
+		{
+			for (int x = 0; x < tImage.Width; x++)
+			{
+				for (int y = 0; y < tImage.Height; y++)
+				{
+					Color tCol = tImage.GetPixel(x, y);
+
+					// L = 0.2126·R + 0.7152·G + 0.0722·B 
+					double L = 0.2126 * tCol.R + 0.7152 * tCol.G + 0.0722 * tCol.B;
+					tImage.SetPixel(x, y, Color.FromArgb(Convert.ToInt32(L), Convert.ToInt32(L), Convert.ToInt32(L)));
+				}
+			}
+		}
+
+		public static Bitmap CreateNonIndexedImage(System.Drawing.Image src)
+		{
+			Bitmap newBmp = new Bitmap(src.Width, src.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+			using (Graphics gfx = Graphics.FromImage(newBmp))
+			{
+				gfx.DrawImage(src, 0, 0);
+			}
+
+			return newBmp;
+		}
+	}
+}
---
+++ Image.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biometrics
+{
+    internal class Image
+    {
+
+        public int ResolutionX;
+        public int ResolutionY;
+        public string name;
+        public Bitmap image;
+
+        public Image(int resolutionX, int resolutionY, string name)
+        {
+            ResolutionX = resolutionX;
+            ResolutionY = resolutionY;
+            this.name = name;
+
+            image = new Bitmap(resolutionY, resolutionX);
+        }
+
+        public Image(string file)
+        {
+            image = Helpers.CreateNonIndexedImage(System.Drawing.Image.FromFile("your image.tif"));
+            ResolutionX = image.Height;
+            ResolutionY = image.Width;
+
+            this.name = file;
+        }
+
+        public void SaveImage()
+        {
+            image.Save(name);
+        }
+    }
+}
